987fa6b3ab469127d5d503441ad766e62fda573b
Hữu Hà <business@zsmart.tech>
2021-11-05 05:43:26 +0700

    test

--- cmd/medusa-api/main.go
+++ cmd/medusa-api/main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"github.com/anti-lgbt/medusa/config"
-	"github.com/anti-lgbt/medusa/models"
 	"github.com/anti-lgbt/medusa/routes"
+	"gorm.io/gorm"
 )
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
 
 func main() {
 	config.InitializeConfig()
 
 	config.Database.AutoMigrate(
-		&models.UserAbc{},
+		&Product{},
 	)
 
 	r := routes.SetupRouter()
