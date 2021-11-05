01cdaea6c65926c1973a8c5b5d990db4ee260a2f
Hữu Hà <business@zsmart.tech>
2021-11-05 10:48:31 +0700

    Fix

--- models/music_album.go
+++ models/music_album.go
@@ -5,7 +5,7 @@ import "time"
 type MusicAlbum struct {
 	ID        int64     `json:"id" gorm:"primaryKey"`
 	MusicID   int64     `json:"music_id" gorm:"type:bigint;not null;uniqueIndex:idx_music_id_and_album_id;index"`
-	AlbumID   int64     `json:"album_id" gorm:"type:bigint;not null;uniqueIndex:idx_strategy_id_and_market_id;index"`
+	AlbumID   int64     `json:"album_id" gorm:"type:bigint;not null;uniqueIndex:idx_music_id_and_album_id;index"`
 	CreatedAt time.Time `json:"created_at" gorm:"type:timestamp(0);not null;index"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"type:timestamp(0);not null;index"`
 	Music     *Music
