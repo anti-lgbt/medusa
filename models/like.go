d90ab0c768083b68c47173752c4ccda0321d82fe
Hữu Hà <business@zsmart.tech>
2021-11-05 08:47:09 +0700

    Fix

--- models/like.go
+++ models/like.go
@@ -10,10 +10,10 @@ import (
 type Like struct {
 	ID        int64               `gorm:"primaryKey"`
 	UserID    int64               `gorm:"type:bigint;not null;uniqueIndex:idx_user_id_and_reply_id_and_comment_id_and_music_id_and_album_id"`
-	ReplyID   datatypes.NullInt64 `gorm:"type:bigint;not null;uniqueIndex:idx_user_id_and_reply_id_and_comment_id_and_music_id_and_album_id"`
-	CommentID datatypes.NullInt64 `gorm:"type:bigint;not null;uniqueIndex:idx_user_id_and_reply_id_and_comment_id_and_music_id_and_album_id"`
-	MusicID   datatypes.NullInt64 `gorm:"type:bigint;not null;uniqueIndex:idx_user_id_and_reply_id_and_comment_id_and_music_id_and_album_id"`
-	AlbumID   datatypes.NullInt64 `gorm:"type:bigint;not null;uniqueIndex:idx_user_id_and_reply_id_and_comment_id_and_music_id_and_album_id"`
+	ReplyID   datatypes.NullInt64 `gorm:"type:bigint;uniqueIndex:idx_user_id_and_reply_id_and_comment_id_and_music_id_and_album_id"`
+	CommentID datatypes.NullInt64 `gorm:"type:bigint;uniqueIndex:idx_user_id_and_reply_id_and_comment_id_and_music_id_and_album_id"`
+	MusicID   datatypes.NullInt64 `gorm:"type:bigint;uniqueIndex:idx_user_id_and_reply_id_and_comment_id_and_music_id_and_album_id"`
+	AlbumID   datatypes.NullInt64 `gorm:"type:bigint;uniqueIndex:idx_user_id_and_reply_id_and_comment_id_and_music_id_and_album_id"`
 	CreatedAt time.Time           `gorm:"type:timestamp(0);not null;index"`
 	UpdatedAt time.Time           `gorm:"type:timestamp(0);not null;index"`
 }
--- models/music_album.go
+++ models/music_album.go
@@ -4,8 +4,8 @@ import "time"
 
 type MusicAlbum struct {
 	ID        int64     `json:"id" gorm:"primaryKey"`
-	MusicID   int64     `json:"music_id" gorm:"uniqueIndex:idx_music_id_and_album_id;index"`
-	AlbumID   int64     `json:"album_id" gorm:"uniqueIndex:idx_strategy_id_and_market_id;index"`
+	MusicID   int64     `json:"music_id" gorm:"type:bigint;not null;uniqueIndex:idx_music_id_and_album_id;index"`
+	AlbumID   int64     `json:"album_id" gorm:"type:bigint;not null;uniqueIndex:idx_strategy_id_and_market_id;index"`
 	CreatedAt time.Time `json:"created_at" gorm:"type:timestamp(0);not null;index"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"type:timestamp(0);not null;index"`
 }
